21bb2ac58678880054638bd457d1fa7081a07202
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-01-24 22:49:16 -0300

    Refatorado o código dos testes de reajustes

--- tdd-java/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ tdd-java/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -12,33 +13,33 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 class ReajusteServiceTest {
 
+	private ReajusteService reajusteService;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void iniciarAtributos(){
+		reajusteService = new ReajusteService();
+		funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+	}
+
 	@Test
 	public void reajusteDeTresPorCentoQuandoDesempenhoForADesejar() {
-		ReajusteService reajusteService = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
 		
 		reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeQuinzePorCentoQuandoDesempenhoForADesejar() {
-		ReajusteService reajusteService = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
 		
 		reajusteService.concederReajuste(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeVintePorCentoQuandoDesempenhoForADesejar() {
-		ReajusteService reajusteService = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
 		
 		reajusteService.concederReajuste(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 
